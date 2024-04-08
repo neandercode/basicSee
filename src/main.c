7241d8a13b75052277f57d5b980a7527e3cb8d4a
neandercode <josephjones555@yahoo.com>
2024-04-08 05:16:02 -0400

    with \n they are now on new lines

--- src/main.c
+++ src/main.c
@@ -2,8 +2,8 @@
 
 int main()
 {
-    printf("Hello World!");
-    printf("this is a second line");
+    printf("Hello World!\n");
+    printf("this is a second line\n");
     printf("this is a third line");
     return 0;
 }
