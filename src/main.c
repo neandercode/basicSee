1fb2db988059bb35334d681b63074367cc74a500
neandercode <josephjones555@yahoo.com>
2024-04-09 05:42:15 -0400

    added float and letter specifiers

--- src/main.c
+++ src/main.c
@@ -3,10 +3,14 @@
 int main()
 {
     int myNum = 42;
+    float myFloatNum = 59.99;
+    char myLetter = 'J';
     printf("Hello World!\n");
     printf("this is a second line\n");
     printf("this is a third line\n");
     //this is a comment
-    printf("%d", myNum);
+    printf("%d\n", myNum);
+    printf("%f\n", myFloatNum);
+    printf("%c\n", myLetter);
     return 0;
 }
