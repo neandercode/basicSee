1dc7088735c444f6ce0ef2f43730dbe56b03ebc2
neandercode <josephjones555@yahoo.com>
2024-04-09 05:31:00 -0400

    how to print an integer

--- src/main.c
+++ src/main.c
@@ -2,9 +2,11 @@
 
 int main()
 {
+    int myNum = 42;
     printf("Hello World!\n");
     printf("this is a second line\n");
-    printf("this is a third line");
+    printf("this is a third line\n");
     //this is a comment
+    printf("%d", myNum);
     return 0;
 }
