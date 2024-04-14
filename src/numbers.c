e1b75aa411be1b22f643742e0aa595874a47a13e
neandercode <josephjones555@yahoo.com>
2024-04-14 07:11:27 -0400

    added double

--- src/numbers.c
+++ src/numbers.c
@@ -3,8 +3,11 @@
 int main() {
     int myNum = 9000;
     float myFlo = 99.5;
+    double myDub = 19.99;
     printf("%f", myFlo);
     printf("\n");
     printf("%d", myNum);
+    printf("\n");
+    printf("%lf", myDub);
     return 0;
 }
