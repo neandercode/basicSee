d88392a9094ce740e6c56f9e634861383203dbd1
neandercode <josephjones555@yahoo.com>
2024-04-17 20:25:05 -0400

    age check

---
+++ src/ifElse.c
@@ -0,0 +1,13 @@
+#include <stdio.h>
+
+int main() {
+    int myAge = 25;
+    int votingAge = 18;
+
+    if (myAge >= votingAge) {
+        printf("congrats, old timer!");
+    } else {
+        printf("need some more hair on your chest, young buck!");
+    }
+    return 0;
+}
