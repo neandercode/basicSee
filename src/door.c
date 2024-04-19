dc57e7121b34f41139a7bba6bac57c079f3d4154
neandercode <josephjones555@yahoo.com>
2024-04-19 05:03:32 -0400

    door code

---
+++ src/door.c
@@ -0,0 +1,13 @@
+#include <stdio.h>
+
+int main() {
+
+    int doorCode = 1234;
+
+    if (doorCode == 1234) {
+        printf("Access granted. \nDoor will now open");
+    } else {
+        printf("Access denied. \nStay cold, my friend");
+    }
+    return 0;
+}
