3e7eac0e042e89d9cba8bf42fb1a8fe350a99dbe
neandercode <josephjones555@yahoo.com>
2024-04-29 05:09:40 -0400

    using the size of operator

---
+++ src/size.c
@@ -0,0 +1,11 @@
+#include <stdio.h>
+
+int main() {
+    int myNumbers[] = {12, 42, 05, 07};
+    int i;
+
+    for (i = 0; i < 4; i++) {
+    printf("%lu", sizeof(myNumbers));
+    }
+    return 0;
+}
