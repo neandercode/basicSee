d986e9334dcc95db17109a3b4b31220db2eee7fc
neandercode <josephjones555@yahoo.com>
2024-04-30 05:23:34 -0400

    its 9

--- src/multiDim.c
+++ src/multiDim.c
@@ -1,7 +1,10 @@
 #include <stdio.h>
-int main() {
-    int matrix[2][3] = { {1, 4, 2}, {3, 6, 8} };
-    printf("%d", matrix[0] [2]);
+
+int main()
+{
+    int matrix[2][3] = {{1, 4, 2}, {3, 6, 8}};
+    matrix[0][0] = 9;
+    printf("%d", matrix[0][0]);
 
     return 0;
 }
